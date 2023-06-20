8a7fc9a31ecadceaf46c4f6ad33afec001ba99ef
romulo114 <romulo.dev114@gmail.com>
2023-06-20 11:52:43 -0300

    feat: check if it returns the provided partitionKey

--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,15 @@
+const crypto = require("crypto");
 const { deterministicPartitionKey } = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
+  });
+
+  it("Returns the provided partitionKey when given a valid string", () => {
+    const input = { partitionKey: "test-key" };
+    const output = deterministicPartitionKey(input);
+    expect(output).toBe("test-key");
   });
 });
